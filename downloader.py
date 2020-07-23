5092a59f710eb9736c00a5653d0ba16b637cb014
firdavsxon <akilovfs@gmail.com>
2020-07-23 12:39:34 -0700

    downloader

--- downloader.py
+++ downloader.py
@@ -1,41 +1,63 @@
-from pytube import YouTube
-from bs4 import BeautifulSoup
+from pytube import YouTube, Playlist
+import os
+import re
 import requests
-from pytube import Playlist
+from bs4 import BeautifulSoup
 
-def scrap_info(url):
-    r = requests.get(url)
-    s = BeautifulSoup(r.text, "html.parser")
-    title = s.find('title').text.strip('- YouTube')
-    # views = s.find("div", class_="watch-view-count").text
-    # likes = s.find("span", class_="like-button-renderer").span.button.text
-    data = {'title': title}
-    return title
+def download_audio(url, destination):
+    if 'http' not in url:
+        url = 'https://www.youtube.com/watch?v=%s' % url
+    yt = YouTube(url)
+    dw = yt.streams.filter(only_audio=True).first()
+    title = yt.title
+    print(f"{title} downloading ...")
+    try:
+        dw.download(destination)
+        print("Done!")
+    except:
+        print(f"Downloading {title} failed")
 
 def download_audio_from_user_input():
     url = input("Enter your url to download: ")
+    if 'http' not in url:
+        url = 'https://www.youtube.com/watch?v=%s' % url
     yt = YouTube(url)
-    print(yt.streams.filter(only_audio=True))
-    user_input = input("Choose itag number to download: ")
-    dw = yt.streams.filter(only_audio=True).get_by_itag(user_input)
-    print("Downloading ..")
+    dw = yt.streams.filter(only_audio=True).first()
+    title = yt.title
+    print(f"{title} downloading ...")
     dw.download('/Users/akilov/Desktop/mp3/Pytube_Download')
     print("Done!")
 
 def download_video_from_user_input():
     url = input("Enter your YouTube video link to download video: ")
+    if 'http' not in url:
+        url = 'https://www.youtube.com/watch?v=%s' % url
     yt = YouTube(url)
-    print(yt.streams)
-    user_input = int(input("Choose resolution number to download: "))
-    dw = yt.streams.get_by_itag(user_input)
-    print("Downloading ..")
+    dw = yt.streams.first()
+    title = yt.title
+    print(f"{title} downloading ..")
     dw.download('/Users/akilov/Desktop/mp3/Pytube_Download')
     print("Done!")
+
+
+def download_video(url, destination):
+    if 'http' not in url:
+        url = 'https://www.youtube.com/watch?v=%s' % url
+    yt = YouTube(url)
+    dw = yt.streams.first()
+    title = yt.title
+    print(f"{title} downloading ..")
+    try:
+        dw.download(destination)
+        print("Done!")
+    except:
+        print(f"Downloading {title} failed")
 
 def download_from_list():
     video_list = ['https://youtu.be/5wyW-w1ikK0?list=PLjzeyhEA84sS6ogo2mXWdcTrL2HRfJUv8',
                   'https://youtu.be/2zToEPpFEN8?list=PLjzeyhEA84sS6ogo2mXWdcTrL2HRfJUv8']
-    user = int(input("1. Audio\n2. Video\n Choose: "))
+    user = int(input("Choose which format you want to download.\n"
+                     "1. Audio\n2. Video\n Choose: "))
 
     if user == 1:
         for i in video_list:
@@ -52,16 +74,34 @@ def download_from_list():
             print(f" Video with views - {title}  .....Downloaded")
     return
 
-# def download_playlist():
-#     pl_list = input("Enter playlist url: ")
-#     playlist = Playlist(pl_list)
-#     # pl.download_all()
-#     # or if you want to download in a specific directory
-#     print(playlist)
-#     print(playlist.video_urls)
-#     # pl.download_all()
+def download_playlist():
+    pl_list = input("Enter playlist url: ")
+    playlist = Playlist(pl_list)
+    playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
+    page = requests.get(pl_list)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    playlist_name = soup.find('title').text.strip(' - YouTube')
+    MYDIR = (f"/Users/akilov/Desktop/mp3/Pytube_Download/{playlist_name}")
+    CHECK_FOLDER = os.path.isdir(MYDIR)
+    if not CHECK_FOLDER:
+        os.makedirs(MYDIR)
+        print("created folder : ", MYDIR)
+    else:
+        print(MYDIR, "folder already exists.")
+    audio_or_video = int(input("Download video as\n 1. Audio\n 2. Video?\n"
+                           "Please choose format: "))
+    if audio_or_video == 1:
+        for url in playlist:
+            download_audio(url, MYDIR)
+    elif audio_or_video == 2:
+        for url in playlist:
+            download_video(url, MYDIR)
+
+
+
+
 
 # download_audio_from_user_input()
 # download_video_from_user_input()
-download_from_list()
-# download_playlist()
+# download_from_list()
+download_playlist()
